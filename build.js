40bb2f4239ba36f743d758f387abbbbdec8d4a37
Erin Dachtler <download333@gmail.com>
2015-06-24 13:55:25 -0700

    Second commit

--- build.js
+++ build.js
@@ -1,28 +1,67 @@
 var fs = require('fs');
 
+var path = require('path');
 
-var header = loadTemplate('header').contents;
-
-var footer = loadTemplate('footer').contents;
+var uglifycss = require('uglifycss');
 
 
-['index', 'menu', 'map', 'feedback', 'contact']
-.map(loadTemplate)
-.map(render)
-.forEach(function(file) {
-	fs.writeFileSync(__dirname + '/public/' + file.name + '.html', file.contents);
-});
+var header = loadTemplate('header');
+
+var footer = loadTemplate('footer');
+
+
+[
+	'index',
+	'menu',
+	'map',
+	'feedback',
+	'contact',
+	'404',
+].forEach(buildHTMLByName);
+
+
+minifyCSS([
+	'public/css/reset.css',
+	'public/css/common.css',
+	'public/css/home.css',
+	'public/css/feedback.css',
+	'public/css/contact.css',
+], 'public/css/compiled.min.css');
+
+minifyCSS([
+	'public/css/common_520.css',
+	'public/css/home_520.css',
+], 'public/css/compiled_520.min.css');
 
 
 function loadTemplate(name) {
-	return {
-		name: name,
-		contents: fs.readFileSync(__dirname + '/templates/' + name + '.html', 'utf8')
-	};
+	return fs.readFileSync(__dirname + '/templates/' + name + '.html', 'utf8');
 }
 
 function render(template) {
 	template.contents = header + template.contents + footer;
 
 	return template;
+}
+
+function buildHTMLByName(name) {
+	buildHTML('templates/' + name + '.html', 'public/' + name + '.html');
+}
+
+function buildHTML(inputFile, outputFile) {
+	var contents = fs.readFileSync(inputFile, 'utf8');
+
+	contents = header + contents + footer;
+
+	fs.writeFileSync(path.resolve(__dirname, outputFile), contents);
+}
+
+function minifyCSS(inputFiles, outputFile) {
+	inputFiles = inputFiles.map(function(file) {
+		return path.resolve(__dirname, file);
+	});
+
+	var uglified = uglifycss.processFiles(inputFiles);
+
+	fs.writeFileSync(path.resolve(__dirname, outputFile), uglified);
 }
--- static-files.js
+++
@@ -1,8 +0,0 @@
-var createDirHandler = require('file-mw');
-
-
-module.exports = createDirHandler(__dirname + '/public', {
-	buffer: true,
-	watch: true
-	// TODO: Use cache
-});
