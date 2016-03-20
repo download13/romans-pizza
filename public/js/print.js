79e38244fbb813cbdbfb5408c52eec5fc6b91d88
Erin Dachtler <download333@gmail.com>
2016-03-20 16:04:01 -0700

    Ignore images and better print

--- public/js/print.js
+++ public/js/print.js
@@ -1,16 +1,11 @@
+var popUpWindow = (function() {
+	var popUpWin = null;
 
-var popUpWin=0;
-function popUpWindow(URLStr, left, top, width, height)
-{
-  if(popUpWin)
-  {
-    if(!popUpWin.closed) popUpWin.close();
-  }
-  popUpWin
-      = open(URLStr,
-  'popUpWin',
-  'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=yes, width='
-      +width+
-      //', height='+height+
-      ', left='+left+', top='+top+', screenX='+left+', screenY='+top+'');
-}
+	return function(URLStr, left, top, width, height) {
+		if(popUpWin && !popUpWin.closed) {
+			popUpWin.close();
+		}
+
+		popUpWin = open(URLStr, 'popUpWin', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=yes, width=' + width + ', left=' + left + ', top=' + top + ', screenX=' + left + ', screenY=' + top);
+	}
+})();
